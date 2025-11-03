cf14d15bf2e10e8b8920ae04f46ca9aea1179ec9
Belvedera-x <belvederastars@gmail.com>
2025-11-03 17:28:13 +0100

    Add hausaufgabe 7

--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from test_app.views import home_page
+from test_app.views import name_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_page),
+    path('<str:user_name>', name_page)
 ]
--- test_app/views.py
+++ test_app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
 
-# Create your views here.
+
+def home_page(request: HttpRequest):
+    return HttpResponse(
+        f"<h1>Hello from our first endpoint!!!</h1>"
+    )
+
+def name_page(request: HttpRequest, user_name):
+    return HttpResponse(
+        f"<h1>Hello {user_name}!!!</h1>"
+    )
+
